fc9e485c76b2adf9d557593d13a17c838f6b5cb3
Gonzalo Zucca <gonzalozucca_4@outlook.com>
2023-05-16 20:46:35 +0200

    added gitignore

--- index.js
+++
@@ -1,8 +0,0 @@
-const server = require("./src/server")
-
-const PORT = 8000;
-
-server.listen(PORT, ()=>{
-  console.log(`API listening on port ${PORT}`)
-
-})
--- src/controllers/index.js
+++
@@ -1,5 +0,0 @@
-const { catchedAsync } = require("../utils");
-
-module.exports = {
-  getCars: catchedAsync(require("./getCars")),
-};

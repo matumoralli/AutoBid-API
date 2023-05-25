4ce03bbe34d020ea69c5a429bebcbc73ca0045ab
Juani <juanicarrenio@gmail.com>
2023-05-25 16:17:58 -0300

    merging

--- src/services/auctions/index.js
+++ src/services/auctions/index.js
@@ -1,7 +1,6 @@
-const { createAuction } = require("./auctions.service")
-const { fetchAuctions} = require("./auctions.service")
+const { fetchAuctions, createAuction} = require("./auctions.service")
 
-module.exports = {
-  create: async (req) => createAuction(req),
-  list: async () => await fetchAuctions(),
+module.exports = { 
+    list: async () => await fetchAuctions(),
+    create: async (req) => createAuction(req)
 }

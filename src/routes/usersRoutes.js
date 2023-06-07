612638ab686107d30ec9e69d4b0b9f2434288d4e
Juani <juanicarrenio@gmail.com>
2023-06-07 15:20:16 -0300

    endTime, set endTime 7 days later than starting time.

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -3,6 +3,7 @@ const controllers = require("../controllers/users");
 const middlewares = require("../middlewares");
 
 router.get("/", middlewares.jwtCheckAdmin, controllers.getUsers);
+
 //! La ruta de abajo busca un usuario con el mail en la query, y si no lo encuentra, lo crea. Funciona como GET y POST.
 router.post(
   "/user/:email",
@@ -10,6 +11,7 @@ router.post(
   controllers.postUser
 );
 //!
+
 router.put("/ban", middlewares.jwtCheckSuperAdmin, controllers.banUser);
 
 router.post("/populate", controllers.populateDB);

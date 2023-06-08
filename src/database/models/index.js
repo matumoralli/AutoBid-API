13f0c60f6d3a8d2411459b738379677102280b88
sotoezequiel <ezequiel_soto_f4@hotmail.com>
2023-06-08 17:24:32 -0300

    fixed models/index.js

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -4,7 +4,7 @@ module.exports = {
   CarDetail: require('./carDetail.model'),
   Comment: require('./comment.model'),
   Reply: require('./reply.model'),  
-  Payment: require('./payment.model')
+  Payment: require('./payment.model'),
   User: require('./user.model'),
   Credit: require('./credit.model')
 }

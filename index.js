64000c8d4bfa57459d2da2840659e16b428cd6a3
Gonzalo Zucca <gonzalozucca_4@outlook.com>
2023-06-08 14:22:51 +0200

    fix to createCarDetail route

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ Reply.belongsTo(Comment);
 
 
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
 
   server.listen(PORT, () => {
     console.log("Server listening at", PORT);

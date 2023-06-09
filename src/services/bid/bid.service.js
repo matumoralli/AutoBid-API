d47f718b6e753767a524d18dee78a859af15a718
Juani <juanicarrenio@gmail.com>
2023-06-09 13:16:44 -0300

    bids controlled by price, not time

--- src/services/bid/bid.service.js
+++ src/services/bid/bid.service.js
@@ -5,12 +5,32 @@ async function postBid({auctionId, userId, ammount}){
   const auction = await Auction.findByPk(auctionId, {include:{ model: Bid}});
   const user = await User.findByPk(userId)
   const lastBidAmmount = auction.Bids[auction.Bids.length -1].dataValues.ammount
-  const bid = await Bid.create(ammount)
-
-  //relating the bid with the respective user and auction
-  bid.setUser(user.dataValues.id)
-  bid.setAuction(auction.dataValues.id)
-  return(bid)
+  
+  try{
+    if(lastBidAmmount < 50000){
+      if(ammount >= lastBidAmmount + 100){
+        console.log(lastBidAmmount + 100)
+        const bid = await Bid.create({ ammount:ammount })
+        bid.setUser(user.dataValues.id)
+        bid.setAuction(auction.dataValues.id)
+        return(bid)
+      } else {
+        return("The bid has to be more than 100 dollars higher than the last one.")
+      }
+    } else {
+      if(ammount >= lastBidAmmount + 250){
+        const bid = await Bid.create({ ammount:ammount })
+        bid.setUser(user.dataValues.id)
+        bid.setAuction(auction.dataValues.id)
+        bid.save()
+        return(bid)
+      } else {
+        return("The bid has to be more than 250 dollars higher than the last one.")
+      }
+      }
+  } catch (error){
+  console.log("There were an error creating the bid", error.message)
+    }
 }
 
 module.exports = {postBid}

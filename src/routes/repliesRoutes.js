c364fadeb0536cdc26cbadeb8fd17cbc071211cb
Gonzalo Zucca <gonzalozucca_4@outlook.com>
2023-06-20 03:23:33 +0200

    nuevas acciones en services de reply y comment

--- src/routes/repliesRoutes.js
+++ src/routes/repliesRoutes.js
@@ -3,6 +3,7 @@ const controllers = require("../controllers/replies")
 
 
 router.get("/:id", controllers.getReplies);
+router.get("/reply/:id", controllers.getReply);
 router.post("/:commentId", controllers.postReply);
 
 module.exports = router;
